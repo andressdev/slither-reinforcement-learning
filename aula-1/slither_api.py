ed815c266a842ce3a70ffeccd1e3ac5a67537917
Andre Santos <andre@graodegente.com.br>
2020-09-20 21:18:13 -0300

    Aula 1

---
+++ aula-1/slither_api.py
@@ -0,0 +1,35 @@
+
+import pyautogui
+import numpy as np
+
+def fullScreenCapture():
+    capture = pyautogui.screenshot()
+    captureArray = np.array(capture)
+    return captureArray
+
+def beginGame():
+    pyautogui.click(x=970, y=492)
+
+def moveTop():
+    pyautogui.moveTo(966, 237)
+
+def moveDown():
+    pyautogui.moveTo(945, 934)
+
+def moveLeft():
+    pyautogui.moveTo(533, 615)
+
+def moveRight():
+    pyautogui.moveTo(1294, 618)
+
+def moveTopLeft():
+    pyautogui.moveTo(557, 299)
+
+def moveTopRight():
+    pyautogui.moveTo(1400, 300)
+
+def moveDownLeft():
+    pyautogui.moveTo(581, 959)
+
+def moveDownRight():
+    pyautogui.moveTo(1311, 876)
